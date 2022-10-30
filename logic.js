0ab33f11ea7daa2b6d39f86af5a4d050103bc21b
Ruth Baker <ruthmoog@gmail.com>
2022-10-30 15:10:35 +0000

    allow the fishy to be plopped

--- logic.js
+++ logic.js
@@ -2,9 +2,11 @@ function allowDrop(event) {
   event.preventDefault();
 }
 function drag(event) {
+  console.log("drag event");
   event.dataTransfer.setData("text", event.target.id);
 }
 function drop(event) {
+console.log(event.target);
   event.preventDefault();
   var data = event.dataTransfer.getData("text");
   event.target.appendChild(document.getElementById(data));
