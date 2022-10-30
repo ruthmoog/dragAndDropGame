677fb57f2520e27ac0b24f06a7688ee35cb3e273
Ruth Baker <ruthmoog@gmail.com>
2022-10-30 15:37:46 +0000

    add potential for different behaviour based on the type of fish

--- logic.js
+++ logic.js
@@ -3,18 +3,21 @@ function allowDrop(event) {
 }
 function drag(event) {
   console.log("drag event");
-  event.dataTransfer.setData("text", event.target.id);
+  event.dataTransfer.setData("target", event.target.id);
+  event.dataTransfer.setData("type", "freshWater");
 }
 function drop(event) {
 console.log(event.target);
   event.preventDefault();
-  var data = event.dataTransfer.getData("text");
+  var data = event.dataTransfer.getData("target");
+  var type = event.dataTransfer.getData("type");
+  console.log(type);
   event.target.appendChild(document.getElementById(data));
 }
 
 function poison(event) {
     console.log(event);
-    var data = event.dataTransfer.getData("text");
+    var data = event.dataTransfer.getData("target");
     document.getElementById(data).className="poisoned";
     drop(event);
 }
